1892da8170f61375e3aa9a8e065aa70a69a80a63
seokmin <meonsik@gmail.com>
2022-04-28 17:02:14 +0900

    hello added

---
+++ Hello/src/test/MainClass.java
@@ -0,0 +1,12 @@
+package test;
+
+public class MainClass {
+	//run(실행)했을 때 실행의 흐름이 시작되는 main method
+	public static void main(String[]args) {
+		System.out.println("Hello,World!");
+	
+		String str="kim";
+		int num1=10;
+		boolean isRun=true;
+	}
+}
